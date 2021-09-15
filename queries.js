ee734cc3dc78fc29df10c64877a0b29640965222
Luke Schuyler <lukeschuyler1@gmail.com>
2021-09-14 18:24:35 -0700

    String interpolation in queries

--- queries.js
+++ queries.js
@@ -1,13 +1,13 @@
 function ChannelReportQuery(pool, artist) {
-  return pool.query("SELECT distinct(trans_type_description), SUM(quantity) as quantity, SUM(label_share_net_receipts) as revenue from main where artist_name =" + "'" + artist + "'"+ " and period = '2021M3' and quantity > 0 group by trans_type_description")
+  return pool.query(`SELECT distinct(trans_type_description), SUM(quantity) as quantity, SUM(label_share_net_receipts) as revenue from main where artist_name = '${artist}' and period = '2021M3' and quantity > 0 group by trans_type_description`)
 }
 
 function SourceReportQuery(pool, artist) {
-  return pool.query("SELECT distinct(retailer), SUM(label_share_net_receipts) as revenue from main where artist_name =" + "'" + artist + "'"+ " and period = '2021M3' and label_share_net_receipts > 0.00000001 group by retailer")
+  return pool.query(`SELECT distinct(retailer), SUM(label_share_net_receipts) as revenue from main where artist_name = '${artist}' and period = '2021M3' and label_share_net_receipts > 0.00000001 group by retailer`)
 }
 
 function DataDumpQuery(pool, artist) {
-  return pool.query("SELECT * from main where artist_name =" + "'" + artist + "'"+ " and period = '2021M3' order by retailer, orchard_upc, product_name,track_name limit 10")
+  return pool.query(`SELECT * from main where artist_name = '${artist}' and period = '2021M3' order by retailer, orchard_upc, product_name,track_name limit 10`)
 }
 
 module.exports = {
